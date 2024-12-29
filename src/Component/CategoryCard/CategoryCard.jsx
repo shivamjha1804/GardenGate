2c10e7a7d6d2a53de968ec2aa696f26a719f242c
shivamjha1804 <jhashivam1804@gmail.com>
2024-12-29 14:45:26 +0530

    up

--- src/Component/CategoryCard/CategoryCard.jsx
+++ src/Component/CategoryCard/CategoryCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CategoryCard = ({ item }) => {
   return (
-    <div className="bg-white relative   overflow-hidden flex flex-col items-center p-4 cursor-pointer">
+    <div className="bg-white relative   overflow-hidden flex flex-col items-center p-1 pt-0 cursor-pointer">
       <img
         className="w-30 h-30 object-cover rounded-lg"
         src={item.image}
